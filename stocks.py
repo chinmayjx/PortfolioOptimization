1f30f25d36ecf8cfe06fe38428ad2c09d86a1950
Chinmay Jain <Chinmayjain08@gmail.com>
2021-01-26 05:21:49 +0530

    ltns

--- stocks.py
+++ stocks.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 index = 0
 files = os.listdir("stkdata/")
+filesx = files.copy()
 for i in range(len(files)):
     files[i] = "stkdata/" + files[i]
 
@@ -21,10 +22,15 @@ def next_stock():
     else:
         return False
 
-
+def get_by_name(n):
+    for x in filesx:
+        if(n+".csv"==x):
+            return pd.read_csv("stkdata/"+n+".csv").dropna()["Close Price"]
+    return None
 def get_name():
     return files[index].split(".")[0]
 
 
 def get_only_name():
     return get_name().split(".")[0].split("/")[1]
+
